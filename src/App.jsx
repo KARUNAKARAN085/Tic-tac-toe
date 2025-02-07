7f087067221d371a44cadcfc7d2cd91487a0c0bf
KARUNAKARAN085 <karunakaranrk2001@gmail.com>
2025-02-07 11:17:13 +0530

    done rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import TicTacToe from './Components/TicTacToe'
+import TicTacToe from './Components/Tictactoegame'
 
 function App() {
 
